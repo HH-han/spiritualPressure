8fd32adb176c7d9724b77578d97a43837bfc992c
bolan <2560177364@qq.com>
2025-08-06 10:48:21 +0800

    main

--- src/views/MyCenter/OrderDetails.vue
+++ src/views/MyCenter/OrderDetails.vue
@@ -437,9 +437,6 @@ onMounted(() => {
 </script>
 
 <style scoped>
-@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@400;500;700&display=swap');
-@import url('https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css');
-
 * {
   margin: 0;
   padding: 0;
